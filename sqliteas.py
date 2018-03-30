e655653e88efffb7f6bdf60cc4dee8790ef43120
FernandoBuenoLima <fernando.bueno_k@hotmail.com>
2018-03-30 17:13:40 -0300

    Added lib

---
+++ sqliteas.py
@@ -0,0 +1,145 @@
+from contextlib import contextmanager
+import sqlite3
+
+class DataBase: #{
+    def __init__(self, name="data"): #{
+        self.name = name
+    #}
+
+    def __str__(self): #{
+        return "Database[{0}.db, {1}]".format(self.name, "open" if self.open else "closed")
+    #}
+
+    def __repr__(self): #{
+        return str(self)
+    #}
+
+    def open(self): #{
+        if self.open:
+            return
+        try:
+            self.conn = sqlite3.connect(self.name + ".db")
+            self.cursor = self.conn.cursor()
+            self.open = True
+        except sqlite3.Error as error:
+            print("Error opening database with name %s" % dbName)
+            print(error)
+    #}
+
+    def commit(self): #{
+        try:
+            self.conn.commit()
+        except sqlite3.Error as error:
+            print(error)
+    #}
+
+    def closeWithoutCommitting(self): #{
+        if not self.open:
+            return
+
+        try:
+            self.cursor.close()
+            self.conn.close()
+            self.open = False
+        except sqlite3.Error as error:
+            print(error)
+    #}
+
+    def close(self): #{
+        if not self.open:
+            return
+            
+        try:
+            self.commit()
+            self.cursor.close()
+            self.conn.close()
+            self.open = False
+        except sqlite3.Error as error:
+            print(error)
+    #}
+
+    @contextmanager
+    def openDB(dbName="data"): #{
+        db = DataBase(dbName)
+        yield db
+        db.closeWithoutCommitting()
+    #}
+
+    def execute(self, query): #{
+        if len(query) > 0 and self.open:
+            try:
+                self.cursor.execute(query)
+            except sqlite3.Error as error:
+                print(error)
+        elif not self.open:
+            print("Cant query a closed DB")
+    #}
+
+    def executeQuery(self, query): #{
+        if len(query) > 0 and self.open:
+            try:
+                self.cursor.execute(query)
+                return self.cursor.fetchall()
+            except sqlite3.Error as error:
+                print(error)
+        elif not self.open:
+            print("Cant query a closed DB")
+    #}
+
+    def createTable(self, name, columns): #{
+        query = "CREATE TABLE IF NOT EXISTS {0}({1});".format(name, columns)
+        print(query)
+        self.execute(query)
+    #}
+
+    def dropTable(self, name): #{
+        query = "DROP TABLE IF EXISTS {0};".format(name)
+        self.execute(query)
+    #}
+
+    def select(self, table, columns="*", where="", orderBy=""): #{
+        query = "SELECT {0} FROM {1}".format(columns, table)
+        if len(where) > 0:
+            query += " WHERE " + where
+        if len(orderBy) > 0:
+            query += " ORDER BY " + orderBy
+        query += ";"
+        return self.executeQuery(query)
+    #}
+
+    def insert(self, table, data): #{
+        query = "INSERT INTO {0} VALUES({1});".format(table, data)
+        self.execute(query)
+    #}
+
+    def insertIntoColumns(self, table, columns, data): #{
+        query = "INSERT INTO {0} ({1}) VALUES({2});".format(table, columns, data)
+        self.execute(query)
+    #}
+
+    def update(self, table, set, where=""): #{
+        query = "UPDATE {0} SET {1}".format(table, set)
+        if len(where) > 0:
+            query += " WHERE " + set
+        query += ";"
+        self.execute(query)
+    #}
+
+    def delete(self, table, where): #{
+        query = "DELETE FROM {0} WHERE ".format(table) + where
+        self.execute(query)
+    #}
+
+    def truncate(self, table): #{
+        self.delete(table, "1")
+    #}
+
+    def listTables(self): #{
+        return self.select("sqlite_master", "name", "type = 'table'")
+    #}
+
+    def listColumns(self, table): #{
+        query = "PRAGMA table_info({0});".format(table)
+        return self.executeQuery(query)
+    #}
+#}
